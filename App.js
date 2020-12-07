d53499e7577df530e11e63f5fd1bba7e4adaf742
Witold Jas <jaswitold1@gmail.com>
2020-12-07 19:05:06 +0100

    setup

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [timerVal,setTimerVal] = useState(new Date())
+   const timer = 
+     setTimeout(() => {
+       setTimerVal(
+         new Date()
+       )
+      
+     }, 1000);
+     
+    
+    
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{timerVal.toLocaleTimeString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
