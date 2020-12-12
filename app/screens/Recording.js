a23fc48d12e5d8f227e9411302bf7f9acfa3b98b
Witold Jas <jaswitold1@gmail.com>
2020-12-12 12:26:11 +0100

    layout recording

--- app/screens/Recording.js
+++ app/screens/Recording.js
@@ -3,15 +3,13 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export default function Recording() {
     return (
-        <View style={styles.container}>
-            <Text>How long You want to record?</Text>
-            <View>
-                <TouchableOpacity>
-                    <Text>
-                        &bull;
-                    </Text>
+        <View style={stylesRec.container}>
+            <Text style={stylesRec.icons}>How long You want to record?</Text>
+            <View style={stylesRec.iconContainer} >
+                <TouchableOpacity style={stylesRec.iconRec}>
+                    <View style={stylesRec.dotRec}></View>
                 </TouchableOpacity>
-                <TouchableOpacity><Text>
+                <TouchableOpacity style={stylesRec.iconRec}><Text style={stylesRec.icons, stylesRec.iconDown}>
                     ↓</Text></TouchableOpacity>
                 
             </View>
@@ -19,10 +17,50 @@ export default function Recording() {
     )
 }
 
-const styles = StyleSheet.create({
+const stylesRec = StyleSheet.create({
     container: {
+        padding:10,
         flex:1,
-        width:'80%'
+        width:'90%',
+        borderRadius:20,
+        backgroundColor:'black',
+        opacity:0.7
     },
+    iconContainer: {
+        flexDirection:'row',
+        alignSelf:'center',
+       
+        alignItems:'center',
+        
+    }, icons: {
+        color:'white',
+        fontSize:20,
+        fontWeight:'200',
+        
+    }, iconRec : {
+        width:70,
+        height:70,
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:100,
+        borderWidth:1,
+        borderColor:'white',
+        borderRadius:10,
+        margin:10
+       
+
+    }, dotRec : {
+        width:30,
+        height:30,
+        backgroundColor:'white',
+        borderRadius:100
+
+    },
+    iconDown: {
+        
+        color:'white',
+        fontSize:30,
+       
+    }
     
 })
